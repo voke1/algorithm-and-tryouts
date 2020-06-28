069bf63d25367d13266bc67196dec9a436b78b2f
Voke1 <vokeolomu@yahoo.com>
2020-06-28 17:23:18 +0100

    solve sock merchant hackerrank challenge

---
+++ sockMerchant.js
@@ -0,0 +1,20 @@
+//Hackerrank Sock Merchant challenge
+
+const sockMerchant = (n, ar) => {
+    let numOfPairs = 0
+    let socks = []
+    ar.forEach((sock) => {
+        if (!socks.includes(sock)) {
+            socks.push(sock)
+        } else {
+            const itemPosition = socks.indexOf(sock)
+            socks.splice(itemPosition)
+            numOfPairs++;
+        }
+    })
+    console.log(numOfPairs)
+    return numOfPairs;
+
+}
+
+sockMerchant(10, [1, 1, 3, 1, 2, 1, 3, 3, 3, 3])  
