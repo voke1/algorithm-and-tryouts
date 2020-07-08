30528261f7f29a72ab64830c63217afeab4650a9
voke1 <vokeolomu@yahoo.com>
2020-07-08 20:50:14 +0100

    solve jumpingOnClouds challenge

---
+++ jumpingOnClouds.js
@@ -0,0 +1,22 @@
+const jumpingOnClouds = (array) => {
+    const indexArray = [];
+    for (let index = 0; index <= array.length; index++) {
+        // if (array[index] !== 0 && index === 1) index += 2;
+         if (array[index] === 0) {
+            indexArray.push(index)
+            index += 2;
+            console.log(indexArray)
+        } else {
+            index -= 1;
+        }
+    }
+    let sum = indexArray.reduce(function (a, b) {
+        return a + b;
+    }, 0);
+
+    console.log('sum:', sum)
+    return sum;
+
+}
+
+jumpingOnClouds([0, 0, 0, 0, 1, 0])
