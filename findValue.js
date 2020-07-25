6b47efbb49364c1bfdf8bb51ec359aef2091abd4
voke1 <vokeolomu@yahoo.com>
2020-07-25 14:30:58 +0100

    add sort method

--- findValue.js
+++ findValue.js
@@ -1,5 +1,11 @@
-const x = [1,2,3,4]
+const x = [1, 2, 3, 4]
 
 x[-1] = -1;
 
 console.log(x[x.indexOf(10000)])
+
+
+//Sort method
+const sortArray = [1, 2, 15, 4, 34, 25]
+const result = sortArray.sort((firstValue, secondValue) => firstValue > secondValue);
+console.log('result: ', result)
