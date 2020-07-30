ca360572e534a22d7e2c1c1a9558e06a3bc9ea16
voke1 <vokeolomu@yahoo.com>
2020-07-30 20:55:11 +0100

    update code

--- test.js
+++ test.js
@@ -83,3 +83,69 @@ const checkFilter = () => {
 }
 
 checkFilter()
+
+function countdown() {
+    // let i; // note we declare let outside of the for loop
+    console.log("Countdown:");
+    for (let i = 5; i >= 0; i--) {
+        setTimeout(function () {
+            console.log(i === 0 ? "GO!" : i);
+        }, (5 - i) * 1000);
+    }
+}
+// countdown();
+
+//Promise
+function countdown(seconds) {
+    try {
+        return new Promise(function (resolve, reject) {
+            for (let i = seconds; i >= 0; i--) {
+                setTimeout(function () {
+                    if (i === 13) return reject(new Error("DEFINITELY NOT COUNTING THAT"));
+                    if (i > 0) console.log(i + '...');
+                    else resolve(console.log("GO!"));
+                }, (seconds - i) * 1000);
+            }
+        });
+
+    } catch (error) {
+        console.log('error happens!')
+    }
+}
+
+// countdown(15)
+
+//Promise using event emitters
+
+const EventEmitter = require('events').EventEmitter;
+
+class Countdown extends EventEmitter {
+    constructor(seconds, superstitious) {
+        super();
+        this.seconds = seconds;
+        this.superstitious = !!superstitious;
+    }
+    go() {
+        const countdown = this;
+        return new Promise(function (resolve, reject) {
+            for (let i = countdown.seconds; i >= 0; i--) {
+                setTimeout(function () {
+                    if (countdown.superstitious && i === 13) {
+                        console.log('reached 14')
+                        return reject(new Error("DEFINITELY NOT COUNTING THAT"));
+                    }
+                    countdown.emit('tick', i);
+                    if (i === 0) resolve();
+                }, (countdown.seconds - i) * 1000);
+            }
+        });
+    }
+}
+
+const newCountdown = new Countdown(13);
+newCountdown.on('tick', function (i) {
+    if (i > 0) console.log(i + '...')
+})
+newCountdown.go()
+    .then(() => console.log('GO!'))
+    .catch(() => console.error(err.message));
